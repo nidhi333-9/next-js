27f5ca1239051e318bfb6881b34760869c2e9743
nidhikumari <kumari9936nidhi@gmail.com>
2022-11-07 10:50:04 +0530

    added the alt property

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,27 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar() {
-    return (
-        <nav>
-            <div className="logo">
-                <Image src="/logo.png" width={50} height={50}></Image>
-            </div>
+  return (
+    <nav>
+      <div className="logo">
+        <Image
+          src="/logo.png"
+          width={50}
+          height={50}
+          alt="logo"
+        ></Image>
+      </div>
 
-            <Link href="/"><a>Home</a></Link>
-            <Link href="/about"><a>About</a></Link>
-            <Link href="/ninjas"><a>The Ninjas List</a></Link>
-
-        </nav>
-    )
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/ninjas">
+        <a>The Ninjas List</a>
+      </Link>
+    </nav>
+  );
 }
